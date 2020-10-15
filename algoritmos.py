36b97f58d1fecbe30af8f95a78c956e70ba167e6
Benjamin Rivas <benjamin.rivas7966@alumnos.udg.mx>
2020-10-14 21:59:11 -0500

    Implementacion de distancia_euclidiana()

--- algoritmos.py
+++ algoritmos.py
@@ -1,3 +1,5 @@
+import math
+
 def distancia_euclidiana(x_1, y_1, x_2, y_2):
     """ Calcula la distancia euclidiana
 
@@ -13,4 +15,4 @@ def distancia_euclidiana(x_1, y_1, x_2, y_2):
     y_2 -- destino_y
 
     """
-    
+    return math.sqrt(math.pow(x_2-x_1,2)+math.pow(y_2-y_1,2))
